2f3104970e45090bdb26db7579e548b0fb68ada8
Matijs van Zuijlen <matijs@matijs.net>
2021-09-24 11:54:03 +0200

    Move RuboCop settings toward compatibility with standardrb
    - Update RuboCop configuration
    - Autocorrect Style/StringLiteralsInInterpolation offenses

--- lib/atspi_app_driver.rb
+++ lib/atspi_app_driver.rb
@@ -25,7 +25,7 @@ module AtspiAccessiblePatches
   end
 
   def inspect_recursive(level = 0, maxlevel = 5)
-    puts "#{'  ' * level} > name: #{name}; role: #{role}"
+    puts "#{"  " * level} > name: #{name}; role: #{role}"
     each_child do |child|
       child.inspect_recursive(level + 1) unless level >= maxlevel
     end
@@ -66,7 +66,7 @@ class AtspiAppDriver
   end
 
   def spawn_process(arguments)
-    command = "ruby -I#{@lib_dir} #{@app_file} #{arguments.join(' ')}"
+    command = "ruby -I#{@lib_dir} #{@app_file} #{arguments.join(" ")}"
     log "About to spawn: `#{command}`"
     @pid = Process.spawn command
   end

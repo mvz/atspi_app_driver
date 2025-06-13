3288a613c9a6462c3888f27c42655c5a1884b54e
Matijs van Zuijlen <matijs@matijs.net>
2025-06-13 12:30:15 +0200

    Autocorrect Minitest/EmptyLineBeforeAssertionMethods

--- test/end_to_end/basic_run_test.rb
+++ test/end_to_end/basic_run_test.rb
@@ -11,27 +11,32 @@ describe "test driving a dummy application" do
   it "starts and can be quit by activating an action" do
     frame = @driver.frame
     button = frame.find_role :push_button
+
     _(button.get_action_name(0)).must_equal "click"
     button.do_action 0
 
     status = @driver.cleanup
+
     _(status.exitstatus).must_equal 0
   end
 
   it "provides access to the main application Atspi object" do
     app = @driver.application
+
     _(app.role).must_equal :application
 
     button = app.find_role :push_button
     button.do_action 0
 
     status = @driver.cleanup
+
     _(status.exitstatus).must_equal 0
   end
 
   it "provides access to label texts" do
     frame = @driver.frame
     label = frame.find_role :label
+
     _(label.get_text(0, -1)).must_equal "Hello, World!"
 
     button = frame.find_role :push_button
